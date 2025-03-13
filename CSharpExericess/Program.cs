5548f439c8b1d0e1080d19fc3f250ae704399517
Basma Suliman <basmaalsinani97@gmail.com>
2025-03-12 23:14:21 -0700

    Add project files.

---
+++ CSharpExericess/Program.cs
@@ -0,0 +1,165 @@
+﻿namespace CSharpExericess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using System.Runtime.CompilerServices;
+            using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace FirstTask
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                //--------------------------------------------------------------------
+                /*1
+
+              int Number;
+              Console.WriteLine("Enter the Number: \n");
+              Number = int.Parse(Console.ReadLine());
+             if (Number%2==0)
+             {
+               Console.WriteLine("\n The number is even");
+              }
+               else
+              {
+              Console.WriteLine("\n The number is odd");
+               }
+
+                //----------------------------------------------------------------------
+                /*2
+                int Num1, Num2, Num3;
+                Console.WriteLine("Enter the Num1: \n");
+                Num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the Num2: \n");
+                Num2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the Num3: \n");
+                Num3 = int.Parse(Console.ReadLine());
+
+                int Largest;
+                if (Num1 > Num2 && Num1 > Num3)
+                {
+                    Console.WriteLine("\n The Num1 is largest");
+                }
+                else  if (Num2 > Num1 && Num2 > Num3)
+                {
+                    Console.WriteLine("\n The Num2 is largest");
+                }
+                else 
+                {
+                    Console.WriteLine("\n The Num3 is largest");
+                }
+                */
+
+                //--------------------------------------------------------------
+
+                /* 
+                 * 3
+                 float Temp;
+                 Console.WriteLine("Enter the temperature converter: \n");
+                Temp = float.Parse(Console.ReadLine());
+                double result;
+                result = (Temp * 9 / 5) + 32;
+                Console.WriteLine(result);
+                 */
+                //---------------------------------------------------------------------
+                /*
+                 * 4
+                float price;
+                Console.WriteLine("Enter the price: \n");
+                price = float.Parse(Console.ReadLine());
+                double result;
+                result = price * 10%100;
+                Console.WriteLine(result);
+             */
+                //---------------------------------------------------------------------
+                /*
+                 * 5
+                 int Marks;
+                 Console.WriteLine("Enter your mark: \n");
+                 Marks = int.Parse(Console.ReadLine());
+                 if (Marks >= 90)
+                 {
+                     Console.WriteLine("\n A");
+                 }
+                 else if (Marks >= 80)
+                 {
+                     Console.WriteLine("\n B");
+                 }
+                 else if (Marks >= 70)
+                 {
+                     Console.WriteLine("\n C");
+                 }
+                 else if (Marks >= 60)
+                 {
+                     Console.WriteLine("\n D");
+                 }
+                 else
+                 {
+                     Console.WriteLine("\n F");
+
+                 }
+                 */
+                //-------------------------------------------------------------
+
+                /*
+                 * 6
+               int Num1, Num2;
+               Console.WriteLine("Enter the Number1: \n");
+               Num1 = int.Parse(Console.ReadLine());
+               Console.WriteLine("Enter the Number2: \n");
+               Num2 = int.Parse(Console.ReadLine());
+
+               int n = Num1;
+               Num1 = Num2;
+               Num2 = n;
+               Console.WriteLine("Number After Swap: \n");
+               Console.WriteLine("Number After Swap:"+Num1);
+               Console.WriteLine("Number After Swap:" + Num2);
+               */
+                //---------------------------------------------------------------
+                /*
+                 * 8
+                float Units;
+                Console.WriteLine("Enter the Units: \n");
+                Units = int.Parse(Console.ReadLine());
+                 if (Units < 100)
+                {
+                    Console.WriteLine(Units*0.5);
+                }
+                else if (Units < 300)
+                {
+                    Console.WriteLine(Units * 0.75 );
+
+                }
+                else if (Units > 301)
+                {
+                    Console.WriteLine(Units * 1);
+
+                }
+                */
+                //--------------------------------------------------------------
+
+                int Num1, Num2;
+                string Operator;
+                Console.WriteLine("Enter the Number1: \n");
+                Num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the Number2: \n");
+                Num2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the Operator: \n");
+                Operator = int.Parse(Console.ReadLine());
+
+                if (Operator ==  )
+                {
+                    Console.WriteLine(Num1 + Num2);
+
+                }
+            }
+        }
+    }
+
+}
+    }
+}
